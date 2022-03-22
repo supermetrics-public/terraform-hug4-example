4346f6f8ebafbf93fd1a8d592003b6a2f33291fe
Dawit Nida <dawit.nida@supermetrics.com>
2022-03-22 10:36:58 +0200

    Use pinned version of GitHub Actions & TF modules
    Also restruct gcp modules, update workflow to exclude staging branch.

--- cloudflare/demo/hug-me/demo-hug-me-cf-zone/main.tf
+++ cloudflare/demo/hug-me/demo-hug-me-cf-zone/main.tf
@@ -1,11 +1,18 @@
+/**
+terraform {
+  backend "s3" {
+  }
+}
+
 terraform {
   backend "local" {
     path = "../../../.tfstate-cloudflare/demo/hug-me/demo-hug-me-cf-zone/terraform.tfstate"
   }
 }
+*/
 
 module "demo_hug_me_cf_zone" {
-  source = "git@github.com:supermetrics-public/terraform-modules-cicd-example.git//examples/cloudflare-simple-modules/cf-example-zone?ref=master"
+  source = "git@github.com:supermetrics-public/terraform-modules-cicd-example.git//examples/cloudflare-simple-modules/cf-example-zone?ref=v1.0.1"
 
   zone = "demo-hug.me"
   plan = "free"
